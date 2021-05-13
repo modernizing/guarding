7aaddfe78fd0063403f28d523773302d260ff37d
Phodal Huang <h@phodal.com>
2021-05-13 23:05:53 +0800

    feat: use tree sitter as languages parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use tree_sitter::{Parser, Language};
+// extern "C" { fn tree_sitter_c() -> Language; }
+// extern "C" { fn tree_sitter_rust() -> Language; }
+
+extern "C" { fn tree_sitter_javascript() -> Language; }
+
 fn main() {
-    println!("Hello, world!");
+    let mut parser = Parser::new();
+
+    let language = unsafe { tree_sitter_javascript() };
+    parser.set_language(language).unwrap();
+
+    let source_code = "import {sayHi} from './say.js';
+const x = 1;
+console.log(x);";
+    let tree = parser.parse(source_code, None).unwrap();
+
+    println!("{:?}", tree.root_node().to_sexp());
+    println!("{:?}", tree.root_node().child(0));
+    println!("{:?}", tree.root_node().child(1));
 }
