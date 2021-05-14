400c122a2aced877fac88d1d6190c91c4bf27473
Phodal Huang <h@phodal.com>
2021-05-14 21:32:07 +0800

    refactor: move allnodes code to js_ident

--- src/identify/js_ident.rs
+++ src/identify/js_ident.rs
@@ -1,9 +1,40 @@
-use crate::{get_all_nodes, tree_sitter_javascript};
-use tree_sitter::Parser;
+use crate::{tree_sitter_javascript};
+use tree_sitter::{Parser, Tree, Node};
 
 pub struct JsIdent {
 
 }
+
+fn get_all_nodes(tree: &Tree) -> Vec<Node> {
+    let mut result = Vec::new();
+
+    let mut cursor = tree.walk();
+    let mut did_visit_children = false;
+    loop {
+        let node = cursor.node();
+        let is_named = node.is_named();
+        if did_visit_children {
+            if cursor.goto_next_sibling() {
+                did_visit_children = false;
+            } else if cursor.goto_parent() {
+                did_visit_children = true;
+            } else {
+                break;
+            }
+        } else {
+            if is_named {
+                result.push(node);
+            }
+            if cursor.goto_first_child() {
+                did_visit_children = false;
+            } else {
+                did_visit_children = true;
+            }
+        }
+    }
+    return result;
+}
+
 
 impl JsIdent {
     pub fn parse(code: &str) {
@@ -18,12 +49,11 @@ impl JsIdent {
         for node in &nodes_before {
             match node.kind() {
                 "import_statement" => {
-                    println!("{:?}", node.child(0));
-                    println!("{:?}", node.child(1));
-                    println!("{:?}", node.child(2));
-                    println!("{:?}", node.child(3));
-                    let cursor = node.walk();
-                    println!("{:?}", cursor.node());
+                    // println!("{:?}", node.child(0));
+                    // println!("{:?}", node.child(1));
+                    // println!("{:?}", node.child(2));
+                    // println!("{:?}", node.child(3));
+                    // println!("{:?}", node.child_by_field_name("string"));
                 }
                 _ => {}
             }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use tree_sitter::{Language, Tree, Node};
+use tree_sitter::{Language};
+
 // extern "C" { fn tree_sitter_c() -> Language; }
 // extern "C" { fn tree_sitter_rust() -> Language; }
 
@@ -9,43 +10,4 @@ pub mod identify;
 
 fn main() {
 
-}
-
-#[allow(unused_assignments)]
-fn get_all_nodes(tree: &Tree) -> Vec<Node> {
-    let mut result = Vec::new();
-
-    let mut cursor = tree.walk();
-    // let mut needs_newline = false;
-    // let mut indent_level = 0;
-    let mut did_visit_children = false;
-    loop {
-        let node = cursor.node();
-        let is_named = node.is_named();
-        if did_visit_children {
-            if is_named {
-                // needs_newline = true;
-            }
-            if cursor.goto_next_sibling() {
-                did_visit_children = false;
-            } else if cursor.goto_parent() {
-                did_visit_children = true;
-                // indent_level -= 1;
-            } else {
-                break;
-            }
-        } else {
-            if is_named {
-                result.push(node);
-                // needs_newline = true;
-            }
-            if cursor.goto_first_child() {
-                did_visit_children = false;
-                // indent_level += 1;
-            } else {
-                did_visit_children = true;
-            }
-        }
-    }
-    return result;
 }
