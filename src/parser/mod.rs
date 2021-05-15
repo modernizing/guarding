52dcfea2d48e19a2ffe596031f96f009379d92fe
Phodal Huang <h@phodal.com>
2021-05-15 23:28:15 +0800

    feat: make first rule

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -4,23 +4,16 @@ use pest::Parser;
 #[grammar = "parser/ident.pest"]
 struct IdentParser;
 
-pub fn parse() {
-    let pairs = IdentParser::parse(Rule::ident_list, "a1 b2").unwrap_or_else(|e| panic!("{}", e));
-
-    // Because ident_list is silent, the iterator will contain idents
+pub fn parse(code: &str) {
+    // online parser: [https://pest.rs/](https://pest.rs/)
+    let pairs = IdentParser::parse(Rule::start, code).unwrap_or_else(|e| panic!("{}", e));
     for pair in pairs {
-        // A pair is a combination of the rule which matched and a span of input
         println!("Rule:    {:?}", pair.as_rule());
         println!("Span:    {:?}", pair.as_span());
         println!("Text:    {}", pair.as_str());
 
-        // A pair can be converted to an iterator of the tokens which make it up:
         for inner_pair in pair.into_inner() {
-            match inner_pair.as_rule() {
-                Rule::alpha => println!("Letter:  {}", inner_pair.as_str()),
-                Rule::digit => println!("Digit:   {}", inner_pair.as_str()),
-                _ => unreachable!()
-            };
+            println!("{:?}", inner_pair)
         }
     }
 }
@@ -31,6 +24,7 @@ mod tests {
 
     #[test]
     fn should_parse_ident() {
-       parse();
+        let code = "class::name contains \"Controller\";";
+        parse(code);
     }
 }
