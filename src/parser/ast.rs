833258a55b76e2c4e1657913dd0c2b01e7b7aed5
Phodal Huang <h@phodal.com>
2021-05-17 12:12:06 +0800

    build: init module struct

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -1,5 +1,12 @@
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub struct Rule {}
+pub struct Rule {
+    pub ty: RuleType,
+    pub level: RuleLevel,
+    pub scope: RuleScope,
+    pub expr: Expr,
+    pub ops: Operation,
+    pub assert: RuleAssert
+}
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum RuleType {
@@ -7,5 +14,28 @@ pub enum RuleType {
     Layer,
 }
 
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum RuleLevel {
+    Class,
+    Package,
+    Module,
+    Function,
+    File,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum RuleScope {
+    PathDefine(String),
+    Extend(String),
+    Implementation(String),
+    MatchRegex(String),
+}
+
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Expr {}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum Operation {}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum RuleAssert {}
