85c74ec162dc2727545bf18712a07cc2e7b0bc25
Phodal Huang <h@phodal.com>
2021-05-17 22:10:59 +0800

    feat: try to make flows

--- src/main.rs
+++ src/main.rs
@@ -16,3 +16,44 @@ pub mod parser;
 fn main() {
 
 }
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+    use std::fs;
+    use crate::parser;
+    use walkdir::WalkDir;
+    use crate::identify::java_ident::JavaIdent;
+
+    fn test_dir() -> PathBuf {
+        let root_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let test_dir = root_dir.join("_fixtures")
+            .join("java");
+
+        test_dir
+    }
+
+    #[test]
+    fn should_working_in_process() {
+        let path = test_dir().join("size.guarding");
+        let content = fs::read_to_string(path).expect("not file");
+        let vec = parser::parse(content.as_str());
+
+
+        let mut models = vec![];
+        for entry in WalkDir::new(test_dir()) {
+            let entry = entry.unwrap();
+            if entry.file_type().is_file() {
+                let file_name = entry.file_name().to_str().expect("error file name");
+                if file_name.ends_with(".java") {
+                    let content = fs::read_to_string(entry.path()).expect("not such file");
+                    models.push(JavaIdent::parse(content.as_str()));
+                }
+            }
+        }
+
+        println!("{:?}", models);
+        for _rule in vec {
+            // rule.capture(rule);
+        }
+    }
+}
