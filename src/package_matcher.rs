4847ef258d6044da7ac06e0656c6a82eb99d9f14
Phodal Huang <h@phodal.com>
2021-05-19 20:01:14 +0800

    docs: update test data

--- src/package_matcher.rs
+++ src/package_matcher.rs
@@ -40,36 +40,36 @@ mod tests {
     #[test]
     fn should_match() {
         let values = vec![
-            "some.arbitrary.pkg | some.arbitrary.pkg             | true",
-            "some.arbitrary.pkg | some.thing.different           | false",
-            "some..pkg          | some.arbitrary.pkg             | true",
-            "some..middle..pkg  | some.arbitrary.middle.more.pkg | true",
-            "*..pkg             | some.arbitrary.pkg             | true",
-            "some..*            | some.arbitrary.pkg             | true",
-            "*..pkg             | some.arbitrary.pkg.toomuch     | false",
-            "toomuch.some..*    | some.arbitrary.pkg             | false",
-            "*..wrong           | some.arbitrary.pkg             | false",
-            "some..*            | wrong.arbitrary.pkg            | false",
-            "..some             | some                           | true",
-            "some..             | some                           | true",
-            "*..some            | some                           | false",
-            "some..*            | some                           | false",
-            "..some             | asome                          | false",
-            "some..             | somea                          | false",
-            "*.*.*              | wrong.arbitrary.pkg            | true",
-            "*.*.*              | wrong.arbitrary.pkg.toomuch    | false",
-            "some.arbi*.pk*..   | some.arbitrary.pkg.whatever    | true",
-            "some.arbi*..       | some.brbitrary.pkg             | false",
-            "some.*rary.*kg..   | some.arbitrary.pkg.whatever    | true",
-            "some.*rary..       | some.arbitrarz.pkg             | false",
-            "some.pkg           | someepkg                       | false",
-            "..pkg..            | some.random.pkg.maybe.anywhere | true",
-            "..p..              | s.r.p.m.a                      | true",
-            "*..pkg..*          | some.random.pkg.maybe.anywhere | true",
-            "*..p..*            | s.r.p.m.a                      | true"];
+            "some.arbitrary.pkg | some.arbitrary.pkg | true",
+            "some.arbitrary.pkg | some.thing.different | false",
+            "some..pkg | some.arbitrary.pkg | true",
+            "some..middle..pkg | some.arbitrary.middle.more.pkg | true",
+            "*..pkg | some.arbitrary.pkg | true",
+            "some..* | some.arbitrary.pkg | true",
+            "*..pkg | some.arbitrary.pkg.toomuch | false",
+            "toomuch.some..* | some.arbitrary.pkg | false",
+            "*..wrong | some.arbitrary.pkg | false",
+            "some..* | wrong.arbitrary.pkg | false",
+            "..some | some | true",
+            "some.. | some | true",
+            "*..some | some | false",
+            "some..* | some | false",
+            "..some | asome | false",
+            "some.. | somea | false",
+            "*.*.* | wrong.arbitrary.pkg | true",
+            "*.*.* | wrong.arbitrary.pkg.toomuch | false",
+            "some.arbi*.pk*.. | some.arbitrary.pkg.whatever | true",
+            "some.arbi*.. | some.brbitrary.pkg | false",
+            "some.*rary.*kg.. | some.arbitrary.pkg.whatever | true",
+            "some.*rary.. | some.arbitrarz.pkg | false",
+            "some.pkg | someepkg | false",
+            "..pkg.. | some.random.pkg.maybe.anywhere | true",
+            "..p.. | s.r.p.m.a | true",
+            "*..pkg..* | some.random.pkg.maybe.anywhere | true",
+            "*..p..* | s.r.p.m.a | true"];
 
         for value in values {
-            let split = value.split("| ");
+            let split = value.split(" | ");
             let vec = split.collect::<Vec<&str>>();
 
             println!("{:?}", vec);
