3ea040e077d7c92770d1e4b8772a4ff003ec1475
Phodal Huang <h@phodal.com>
2021-05-21 07:53:01 +0800

    feat: add guarding config

--- src/bin/guarding.rs
+++ src/bin/guarding.rs
@@ -1,11 +1,32 @@
 use std::fs;
 use std::path::PathBuf;
 
+use clap::{AppSettings, Clap};
+
 use guarding::rule_executor::RuleExecutor;
 
+#[derive(Clap)]
+#[clap(version = "1.0", author = "Inherd Group <group@inherd.org>")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    #[clap(short, long, default_value = "guarding.guarding")]
+    config: String,
+
+    #[clap(short, long, default_value = "src")]
+    path: String,
+
+    #[clap(short, long, default_value = "guard.json")]
+    output: String,
+}
+
 fn main() {
-    let buf = PathBuf::from(".");
-    let guarding = buf.clone().join("guarding.guarding");
+    let opts: Opts = Opts::parse();
+
+    let buf = PathBuf::from(opts.path);
+    let guarding = PathBuf::from(opts.config);
     let content = fs::read_to_string(guarding).unwrap();
-    RuleExecutor::execute(content, buf);
+
+    let errors = RuleExecutor::execute(content, buf);
+    let content = serde_json::to_string_pretty(&errors).unwrap();
+    let _ = fs::write(opts.output, content);
 }
