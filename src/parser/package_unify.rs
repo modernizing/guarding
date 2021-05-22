e671032bc02df126f271769e9dba33273b84c8aa
Phodal Huang <h@phodal.com>
2021-05-22 12:42:09 +0800

    refactor: inline package unify

--- src/parser/package_unify.rs
+++ src/parser/package_unify.rs
@@ -1,29 +1,33 @@
 use std::path::PathBuf;
 
-pub fn path_to_package(path: PathBuf) -> String {
-    let mut package = "".to_string();
-    let path_iter = path.iter();
-    path_iter.for_each(|str| {
-        match str.to_str() {
-            None => {}
-            Some(sub) => {
-                package = format!("{}.{}", package, sub);
-            }
-        }
-    });
+pub struct PackageUnify {}
 
-    package.remove(0);
-    package
+impl PackageUnify {
+    pub fn from_path(path: PathBuf) -> String {
+        let mut package = "".to_string();
+        let path_iter = path.iter();
+        path_iter.for_each(|str| {
+            match str.to_str() {
+                None => {}
+                Some(sub) => {
+                    package = format!("{}.{}", package, sub);
+                }
+            }
+        });
+
+        package.remove(0);
+        package
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use std::path::PathBuf;
-    use crate::parser::package_unify::path_to_package;
+    use crate::parser::package_unify::PackageUnify;
 
     #[test]
     fn should_convert_path_to_package() {
         let buf = PathBuf::from("src").join("core").join("domain");
-        assert_eq!("src.core.domain".to_string(), path_to_package(buf));
+        assert_eq!("src.core.domain".to_string(), PackageUnify::from_path(buf));
     }
 }
