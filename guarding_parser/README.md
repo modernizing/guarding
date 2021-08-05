322b2a86edd11c03d28f01b99af4b0ba8a5139d3
Phodal Huang <h@phodal.com>
2021-08-05 08:21:59 +0800

    refactor: extract exec

--- guarding_parser/README.md
+++ guarding_parser/README.md
@@ -1,2 +1,7 @@
 # Guarding Parser
 
+usage:
+
+```
+
+```
--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,12 @@ use std::path::PathBuf;
 use guarding_core::rule_executor::{RuleErrorMsg, RuleExecutor};
 use guarding_parser::parser;
 use model_builder::ModelBuilder;
+use guarding_parser::ast::GuardRule;
+use guarding_core::domain::code_file::CodeFile;
+use std::alloc::Global;
 
 pub mod identify;
+pub mod model_builder;
 
 pub fn exec_guarding(rule_content: String, code_dir: PathBuf) -> Vec<RuleErrorMsg> {
     match parser::parse(rule_content.as_str()) {
@@ -16,16 +20,18 @@ pub fn exec_guarding(rule_content: String, code_dir: PathBuf) -> Vec<RuleErrorMs
         },
         Ok(rules) => {
             let models = ModelBuilder::build_models_by_dir(code_dir);
-
-            let mut executor = RuleExecutor::new(models, rules);
-            executor.run();
-
-            return executor.errors;
+            exec(rules, models)
         }
     }
+}
+
+fn exec(rules: Vec<GuardRule>, models: Vec<CodeFile>) -> Vec<RuleErrorMsg> {
+    let mut executor = RuleExecutor::new(models, rules);
+    executor.run();
+
+    return executor.errors;
 }
 
 
 #[cfg(test)]
 mod tests;
-pub mod model_builder;
