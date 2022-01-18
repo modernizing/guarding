86e06a2c7ee8bbf7ae1ebfa1567ae9852008d296
Phodal Huang <h@phodal.com>
2022-01-18 21:30:49 +0800

    feat(c#): init name import

--- guarding_ident/src/identify/c_sharp_ident.rs
+++ guarding_ident/src/identify/c_sharp_ident.rs
@@ -1,27 +1,11 @@
-use tree_sitter::{Parser, Query};
+use tree_sitter::{Node, Parser, Query, QueryCursor};
+use guarding_core::domain::code_class::CodeClass;
+use guarding_core::domain::code_file::CodeFile;
+use crate::code_ident::CodeIdent;
 
 const C_SHARP_QUERY: &'static str = "
-(package_declaration
-	(scoped_identifier) @package-name)
-
-(import_declaration
-	(scoped_identifier) @import-name)
-
-(method_declaration
-	(modifiers
-    	(annotation
-    		name: (identifier) @annotation.name
-            arguments: (annotation_argument_list)? @annotation.key_values
-    	)
-    )
-)
-
-(program
-    (class_declaration
-	    name: (identifier) @class-name
-        interfaces: (super_interfaces (interface_type_list (type_identifier)  @impl-name))?
-    )
-)
+(using_directive
+	(qualified_name) @import-name)
 
 ";
 
@@ -45,11 +29,60 @@ impl CSharpIdent {
             query
         }
     }
+
+    fn do_parse(code: &&str, ident: &mut CSharpIdent) -> CodeFile {
+        let tree = ident.parser.parse(code, None).unwrap();
+        let text_callback = |n: Node| &code[n.byte_range()];
+        let mut query_cursor = QueryCursor::new();
+        let captures = query_cursor.captures(&ident.query, tree.root_node(), text_callback);
+
+        let mut code_file = CodeFile::default();
+        #[allow(unused_mut)]
+        let mut class = CodeClass::default();
+        #[allow(unused_mut)]
+        let mut is_last_node = false;
+
+        let capture_names = ident.query.capture_names();
+
+        for (mat, capture_index) in captures {
+            let capture = mat.captures[capture_index];
+            let capture_name = &capture_names[capture.index as usize];
+
+            let text = capture.node.utf8_text((&code).as_ref()).unwrap_or("");
+            match capture_name.as_str() {
+                "import-name" => {
+                    code_file.imports.push(text.to_string());
+                }
+                &_ => {
+                    println!(
+                        "    pattern: {}, capture: {}, row: {}, text: {:?}",
+                        mat.pattern_index,
+                        capture_name,
+                        capture.node.start_position().row,
+                        capture.node.utf8_text((&code).as_ref()).unwrap_or("")
+                    );
+                }
+            }
+        }
+
+        if is_last_node {
+            code_file.classes.push(class.clone());
+        }
+
+        code_file
+    }
 }
 
+impl CodeIdent for CSharpIdent {
+    fn parse(code: &str) -> CodeFile {
+        let mut ident = CSharpIdent::new();
+        CSharpIdent::do_parse(&code, &mut ident)
+    }
+}
 
 #[cfg(test)]
 mod tests {
+    use crate::code_ident::CodeIdent;
     use crate::identify::c_sharp_ident::CSharpIdent;
 
     #[test]
