f963cb0e7eb7761f63b043999074e2d51ed54687
Phodal Huang <h@phodal.com>
2022-01-18 23:17:37 +0800

    feat(c#): add functions support

--- guarding_ident/src/identify/c_sharp_ident.rs
+++ guarding_ident/src/identify/c_sharp_ident.rs
@@ -1,6 +1,7 @@
 use tree_sitter::{Node, Parser, Query, QueryCursor};
 use guarding_core::domain::code_class::CodeClass;
 use guarding_core::domain::code_file::CodeFile;
+use guarding_core::domain::code_function::CodeFunction;
 use crate::code_ident::CodeIdent;
 
 const C_SHARP_QUERY: &'static str = "
@@ -61,10 +62,10 @@ impl CSharpIdent {
         let mut iters = captures.into_iter();
         while let Some((mat, capture_index)) = iters.next() {
             let capture = mat.captures[capture_index];
-            let capture_name = &capture_names[capture.index as usize];
+            let capture_name: &str = &capture_names[capture.index as usize];
 
             let text = capture.node.utf8_text((&code).as_ref()).unwrap_or("");
-            match capture_name.as_str() {
+            match capture_name {
                 "import-name" => {
                     code_file.imports.push(text.to_string());
                 }
@@ -75,17 +76,18 @@ impl CSharpIdent {
                     let class_node = capture.node.parent().unwrap();
                     CSharpIdent::insert_location(&mut class, class_node);
                 }
-                "body" => {
-                    if !class.name.is_empty() {
-                        code_file.classes.push(class.clone());
-                        class = CodeClass::default();
-                    }
-                }
                 "impl-name" => {
                     class.implements.push(text.to_string());
                 }
                 "method-name" => {
+                    let mut function = CodeFunction::default();
+                    function.name = text.to_string();
+                    class.functions.push(function);
 
+                    if !class.name.is_empty() {
+                        code_file.classes.push(class.clone());
+                        class = CodeClass::default();
+                    }
                 }
                 &_ => {
                     println!(
@@ -146,5 +148,22 @@ mod tests {
         assert_eq!(2, file.classes[0].implements.len());
         assert_eq!("CSharpSyntaxWalker", file.classes[0].implements[0]);
         assert_eq!("DemoInterface", file.classes[0].implements[1]);
+    }
+
+    #[test]
+    fn should_parse_class_functions() {
+        let source_code = "[ApiController]
+public class SharpingClassVisitor {
+  public Domain domain { get; set; }
+  public void VisitClassDeclaration(ClassDeclarationSyntax node){
+
+  }
+}";
+
+        let file = CSharpIdent::parse(source_code);
+        assert_eq!(1, file.classes.len());
+
+        assert_eq!(1, file.classes[0].functions.len());
+        assert_eq!("VisitClassDeclaration", file.classes[0].functions[0].name);
     }
 }
